f43eb8bc0e25c2ffab41740b19698d890235e8d6
XHS <xhs@qq.com>
2019-09-27 11:59:50 +0800

    第3次提交

---
+++ src/main/java/com/qf/controller/teachercontroller.java
@@ -0,0 +1,12 @@
+package com.qf.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @ClassName: teachercontroller
+ * @Author: 53074
+ * Date: 2019/9/27
+ */
+@Controller
+public class teachercontroller {
+}
